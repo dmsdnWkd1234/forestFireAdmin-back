9b946827f2be0571ec6961227de552ff7fa94262
kimeunwoo <gumman0620@gmail.com>
2025-03-24 20:15:37 +0900

    로그인을 요청

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,13 +2,21 @@
 
 const id = document.querySelector('#id');
 const ps = document.querySelector('#password');
-const loginbtn = document.querySelector('#login');
+const loginbtn = document.querySelector('#button');
 
 loginbtn.addEventListener('click', login);
 
-const login = () => {
+function login() {
     const req = {
         id: id.value,
         password: ps.value,
     };
-};
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req),
+    });
+}
