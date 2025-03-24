4dd649af27254aab2b809d656a926e75f947943f
kimeunwoo <gumman0620@gmail.com>
2025-03-24 19:59:20 +0900

    DOM제어

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 'use strict';
 
-console.log('hellooo');
-console.log('sese');
+const id = document.querySelector('#id');
+const ps = document.querySelector('#password');
+const loginbtn = document.querySelector('#login');
+
+loginbtn.addEventListener('click', login);
+
+const login = () => {
+    const req = {
+        id: id.value,
+        password: ps.value,
+    };
+};
