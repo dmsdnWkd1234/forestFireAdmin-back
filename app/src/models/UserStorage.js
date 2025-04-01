8ec7ec90e9dbed20b24d7ff77215065ce55ea087
kimeunwoo <gumman0620@gmail.com>
2025-04-01 13:44:19 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,8 +17,12 @@ class UserStorage {
 
         return userInfo;
     }
-    static getUsers(...fields) {
-        // const users = this.#user;
+
+    static #getUsers(data, isAll, fields) {
+        const users = JSON.parse(data);
+        if (isAll) {
+            return users;
+        }
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -26,6 +30,14 @@ class UserStorage {
             return newUsers;
         }, {});
         return newUsers;
+    }
+    static getUsers(isAll, ...fields) {
+        return fs
+            .readFile('./src/databases/users.json')
+            .then((data) => {
+                return this.#getUsers(data, fields);
+            })
+            .catch(console.error);
     }
 
     static getUserInfo(id) {
@@ -37,11 +49,15 @@ class UserStorage {
             .catch(console.error);
     }
 
-    static save(userInfo) {
-        // const users = this.#user;
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if (users.id.includes(userInfo.id)) {
+            throw '이미 존재하는 아이디';
+        }
         users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
         users.password.push(userInfo.password);
+        users.name.push(userInfo.name);
+        fs.writeFile('./src/databases/users.json', JSON.stringify(users));
         return { success: true };
     }
 }
