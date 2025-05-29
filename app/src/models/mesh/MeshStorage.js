fc6ea2d4ebe77f6d779ec26a71ec7805654befb3
kimeunwoo <gumman0620@gmail.com>
2025-05-29 16:27:16 +0900

    DB 오류 수정

--- app/src/models/mesh/MeshStorage.js
+++ app/src/models/mesh/MeshStorage.js
@@ -3,15 +3,15 @@
 const db = require('../../config/db');
 
 class MeshStorage {
-    static getMeshInfo = () => {
-        const query = 'SELECT * FROM meshs;';
-        return new Promise((resolve, reject) => {
-            db.query(query, (err, data) => {
-                if (err) reject(`${err}`);
-                resolve(data);
-            });
-        });
-    };
+  static getMeshInfo = () => {
+    const query = 'SELECT * FROM mesh;';
+    return new Promise((resolve, reject) => {
+      db.query(query, (err, data) => {
+        if (err) reject(`${err}`);
+        resolve(data);
+      });
+    });
+  };
 }
 
 module.exports = MeshStorage;
