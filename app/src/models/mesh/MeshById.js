b20cd9e3f63e7775cec75a18b96d7b1753f361dc
kimeunwoo <gumman0620@gmail.com>
2025-06-02 21:26:40 +0900

    서버 부하 줄이기 위해meshById 쿼리 수정

--- app/src/models/mesh/MeshById.js
+++ app/src/models/mesh/MeshById.js
@@ -4,7 +4,13 @@ const db = require('../../config/db');
 
 class MeshById {
   static getMeshInfoById = (id) => {
-    const query = `SELECT * FROM mesh WHERE unicast_address = ? ORDER BY Time DESC LIMIT 1;`;
+    const query = `
+      SELECT Temp, Humidity, CO2, TVOC, Pressure, Emergency, Battery_Persent, Voltage, Time
+      FROM mesh
+      WHERE unicast_address = ?
+      ORDER BY Time DESC
+      LIMIT 1;
+    `;
     return new Promise((resolve, reject) => {
       db.query(query, [id], (err, data) => {
         if (err) return reject(`${err}`);
