6154670397d57d2447c3404b181c3f929b4a5fd0
kimeunwoo <gumman0620@gmail.com>
2025-09-16 15:39:36 +0900

    쿼리문 수정

--- app/src/models/mesh/MeshById.js
+++ app/src/models/mesh/MeshById.js
@@ -7,7 +7,6 @@ class MeshById {
     const query = `
       SELECT Temp, Humidity, CO2, TVOC, Pressure, Emergency, Battery_Persent, Voltage, Time
       FROM mesh
-      WHERE unicast_address = ?
       ORDER BY Time DESC
       LIMIT 1;
     `;
