ceefea3f8af95088f0a20fb856fd80cf48c89873
kimeunwoo <gumman0620@gmail.com>
2025-12-01 15:40:16 +0900

    최종수정

--- app/src/models/reports/ReportListStorage.js
+++ app/src/models/reports/ReportListStorage.js
@@ -1,20 +1,20 @@
 'use strict';
 
-// ESM import statement
-// (수정) .js 확장자 추가 필요 (db.js 파일 확인 필요)
 import db from '../../config/db.js';
 
 class ReportListStorage {
   static getReport = () => {
-    const query = 'SELECT * FROM reports;';
+    // 1. WHERE Emergency > 0: 신고(Emergency) 값이 0보다 큰(1인) 녀석들만 가져와.
+    // 2. ORDER BY Time DESC: 가장 최근에 터진 사건부터 위로 오게 정렬해.
+    const query = 'SELECT * FROM mesh WHERE Emergency > 0 ORDER BY Time DESC;';
+
     return new Promise((resolve, reject) => {
       db.query(query, (err, data) => {
         if (err) reject(`${err}`);
-        else resolve(data); // Return data on success
+        else resolve(data);
       });
     });
   };
 }
 
-// (수정) module.exports -> export default
 export default ReportListStorage;
